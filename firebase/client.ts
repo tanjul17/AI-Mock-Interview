7f1cd7de23059b0f6d9b5c7af8528b514fed7c4d
Tanjul Sarathe <97596364+tanjul17@users.noreply.github.com>
2025-03-31 00:50:09 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -25,25 +25,3 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-
-/*// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD1I_EWIC3-tBjYHnl65hp76-enxTli0CY",
-  authDomain: "abhyaasbot.firebaseapp.com",
-  projectId: "abhyaasbot",
-  storageBucket: "abhyaasbot.firebasestorage.app",
-  messagingSenderId: "606005169487",
-  appId: "1:606005169487:web:9aaebe3f9f7f7608ba493a",
-  measurementId: "G-SPYG72FHK4"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);*/
